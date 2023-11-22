3f51ed658868226ed6077727605be2ff40eff5a0
aaqibjavaid <aaqibjavaid605@gmail.com>
2023-11-22 17:27:44 +0530

    complete FAQ section

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ window.addEventListener("scroll", () => {
     .querySelector("nav")
     .classList.toggle("window-scroll", window.scrollY > 0);
 });
+
+// show/hide faq answer
+const faqs = document.querySelectorAll(".faq");
+faqs.forEach((faq) =>
+  faq.addEventListener("click", function () {
+    // Toggle the "open" class
+    faq.classList.toggle("open");
+
+    // Select the <i> element within the clicked .faq
+    const icon = faq.querySelector("i");
+
+    // Check if the "open" class is present
+    if (faq.classList.contains("open")) {
+      // If the "open" class is added, set the class to "fa-minus"
+      icon.classList.replace("fa-plus", "fa-minus");
+    } else {
+      // If the "open" class is removed, set the class to "fa-plus"
+      icon.classList.replace("fa-minus", "fa-plus");
+    }
+  })
+);
