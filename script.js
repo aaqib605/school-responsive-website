3b74ec28199ca2b595bbf137c90b5f5b5168a632
aaqibjavaid <aaqibjavaid605@gmail.com>
2023-11-28 16:34:22 +0530

    complete navbar responsive

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// Change navbar styles on scroll
+// Change Navbar styles on scroll
 window.addEventListener("scroll", () => {
   document
     .querySelector("nav")
     .classList.toggle("window-scroll", window.scrollY > 0);
 });
 
-// show/hide faq answer
+// Show/Hide FAQ answer
 const faqs = document.querySelectorAll(".faq");
 faqs.forEach((faq) =>
   faq.addEventListener("click", function () {
@@ -42,7 +42,27 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-// copyright date
+// Copyright Date
 const today = new Date().getFullYear();
 const copyrightEl = document.querySelector(".footer__copyright span");
 copyrightEl.textContent = today;
+
+// Toggle Navbar
+const openMenuBtn = document.querySelector("#open-menu-btn");
+const closeMenuBtn = document.querySelector("#close-menu-btn");
+const navbarMenu = document.querySelector(".nav__menu");
+
+function openMenu() {
+  navbarMenu.style.display = "inline-block";
+  openMenuBtn.style.display = "none";
+  closeMenuBtn.style.display = "inline-block";
+}
+
+function closeMenu() {
+  navbarMenu.style.display = "none";
+  openMenuBtn.style.display = "inline-block";
+  closeMenuBtn.style.display = "none";
+}
+
+openMenuBtn.addEventListener("click", openMenu);
+closeMenuBtn.addEventListener("click", closeMenu);
